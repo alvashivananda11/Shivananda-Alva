d70fe91020e46ac0f2cb3b5d4e10302e375c3478
alvashivananda11 <alvashivananda11@gmail.com>
2017-12-08 15:42:50 +0530

    update

---
+++ C++/Addition.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+main()
+{
+int a,b,c;
+cout<<"Enter the two number to add \n";
+cin >> a >> b;
+c=a+b;
+cout<<"Sum:"<<c;
+return 0;
+}
---
+++ VB.NET/GUI/picturebox/picturebox/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If PictureBox1.Left < 500 Then
+            PictureBox1.Left = PictureBox1.Left + 5
+        Else
+            PictureBox1.Left = 0
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Timer1.Start()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Timer1.Stop()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        PictureBox1.Left = 60
+        Timer1.Stop()
+    End Sub
+End Class
